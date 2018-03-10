f9165ddf906236c3b09d5edf1e28ac0998afd51b
David Le Bansais <dlebansais@gmail.com>
2018-03-10 15:31:57 +0100

    Changed ProgressBar to use a dedicated control.

---
+++ PgJsonParse/Controls/PercentProgressBar.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CustomControls
+{
+    public partial class PercentProgressBar : UserControl, INotifyPropertyChanged
+    {
+        // List of all existing controls of this type.
+        public static List<PercentProgressBar> ControlList { get; } = new List<PercentProgressBar>();
+
+        // Use NaN to make sure that the control is updated as soon as a correct value is entered.
+        private const double DefaultValue = double.NaN;
+
+        #region Init
+        public PercentProgressBar()
+        {
+            ControlList.Add(this);
+            InitializeComponent();
+
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            NotifyPropertyChanged(nameof(Value));
+        }
+
+        ~PercentProgressBar()
+        {
+            ControlList.Remove(this);
+        }
+        #endregion
+
+        #region Custom properties and events
+        /// <summary>
+        ///     Identifies the <see cref="Value"/> dependency property.
+        /// </summary>
+        /// <returns>
+        ///     The identifier for the <see cref="Value"/> dependency property.
+        /// </returns>
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(double), typeof(PercentProgressBar), new FrameworkPropertyMetadata(DefaultValue, OnValueChanged));
+
+        /// <summary>
+        ///     Gets or sets the value.
+        /// </summary>
+        [Bindable(true)]
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        private static void OnValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            PercentProgressBar ctrl = (PercentProgressBar)d;
+            ctrl.OnValueChanged(e);
+        }
+
+        private void OnValueChanged(DependencyPropertyChangedEventArgs e)
+        {
+            NotifyPropertyChanged(nameof(Value));
+        }
+        #endregion
+
+        #region Implementation of INotifyPropertyChanged
+        /// <summary>
+        ///     Implements the PropertyChanged event.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        internal void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        [SuppressMessage("Microsoft.Design", "CA1026:DefaultParametersShouldNotBeUsed", Justification = "Default parameter is mandatory with [CallerMemberName]")]
+        internal void NotifyThisPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
---
+++ PgJsonParse/Converters/PercentProgressBarPropertyConverter.cs
@@ -0,0 +1,34 @@
+﻿using CustomControls;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Converters
+{
+    public class PercentProgressBarPropertyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double DoubleValue = (double)value;
+            if (double.IsNaN(DoubleValue))
+                DoubleValue = 0;
+
+            string ControlName = parameter as string;
+            foreach (PercentProgressBar Ctrl in PercentProgressBar.ControlList)
+                if (Ctrl.Name == ControlName)
+                {
+                    double Maximum = Ctrl.ActualWidth;
+                    double Margin = Ctrl.ActualWidth * ((100 - DoubleValue) / 100);
+                    return new Thickness(0, 0, Margin, 0);
+                }
+
+            return new Thickness(0, 0, 0, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return false;
+        }
+    }
+}
