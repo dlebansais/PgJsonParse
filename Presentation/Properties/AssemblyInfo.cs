135aed89faec4161f7e52c0f38c357277a69f783
dlebansais <dlebansais@gmail.com>
2018-05-06 13:23:19 +0200

    More cleanup and reorganization.

--- Presentation/Properties/AssemblyInfo.cs
+++ Presentation/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.1729")]
-[assembly: AssemblyFileVersion("1.0.0.120")]
+[assembly: AssemblyVersion("1.1.0.1730")]
+[assembly: AssemblyFileVersion("1.0.0.121")]
