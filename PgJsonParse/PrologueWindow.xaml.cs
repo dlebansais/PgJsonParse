592295d989a2af4ff740d0ca396fb4103594bfab
dlebansais <dlebansais@gmail.com>
2018-11-09 00:49:52 +0100

    Update to client 311.

--- PgJsonParse/PrologueWindow.xaml.cs
+++ PgJsonParse/PrologueWindow.xaml.cs
@@ -20,9 +20,9 @@ namespace PgJsonParse
 {
     public partial class PrologueWindow : RootControl, INotifyPropertyChanged
     {
-        public const double PARSER_VERSION = 310;
-        public const int PG_CACHE_VERSION = 310;
-        public const int PG_CACHE_SIZE = 36904968;
+        public const double PARSER_VERSION = 311;
+        public const int PG_CACHE_VERSION = 311;
+        public const int PG_CACHE_SIZE = 36905032;
 
         #region Init
         public PrologueWindow()
