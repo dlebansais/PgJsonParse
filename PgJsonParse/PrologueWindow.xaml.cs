f7723ab32ec521d69743ed2e4dd7d7aa970d229a
dlebansais <dlebansais@gmail.com>
2019-10-30 20:05:29 +0100

    Fixed area event XAML template.

--- PgJsonParse/PrologueWindow.xaml.cs
+++ PgJsonParse/PrologueWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace PgJsonParse
 {
     public partial class PrologueWindow : RootControl, INotifyPropertyChanged
     {
-        public const double PARSER_VERSION = 327;
+        public const double PARSER_VERSION = 327.1;
         public const int PG_CACHE_VERSION = 327;
         public const int PG_CACHE_SIZE = 41798096;
 
