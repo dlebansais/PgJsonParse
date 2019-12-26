277da82e862d2a28264c1f1b71992dc8d4edf1ed
dlebansais <dlebansais@gmail.com>
2019-12-26 22:04:04 +0100

    Updated to .NET Framework 4.8

--- Presentation/Properties/AssemblyInfo.cs
+++ Presentation/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.4004")]
-[assembly: AssemblyFileVersion("1.0.0.175")]
+[assembly: AssemblyVersion("1.1.0.4005")]
+[assembly: AssemblyFileVersion("1.0.0.176")]
