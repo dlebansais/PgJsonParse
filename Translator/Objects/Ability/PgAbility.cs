e2a4d9bb7e0fa94be3bbc88c69c8498fcb83652a
dlebansais <dlebansais@gmail.com>
2023-06-24 19:27:13 +0200

    Cleanup.

--- Translator/Objects/Ability/PgAbility.cs
+++ Translator/Objects/Ability/PgAbility.cs
@@ -87,10 +87,6 @@
         public float? RawResetTime { get; set; }
         public PgSelfParticle SelfParticle { get; set; } = null!;
         public PgSelfPreParticle SelfPreParticle { get; set; } = null!;
-        public uint AbilityTargetParticleColor0 { get { return RawAbilityTargetParticleColor0.HasValue ? RawAbilityTargetParticleColor0.Value : 0; } }
-        public uint? RawAbilityTargetParticleColor0 { get; set; }
-        public uint AbilityTargetParticleColor1 { get { return RawAbilityTargetParticleColor1.HasValue ? RawAbilityTargetParticleColor1.Value : 0; } }
-        public uint? RawAbilityTargetParticleColor1 { get; set; }
         public string AmmoDescription { get; set; } = string.Empty;
         public string? SharesResetTimerWith_Key { get; set; }
         public string? Skill_Key { get; set; }
